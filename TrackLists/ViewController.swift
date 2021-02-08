a918563ab2e27e3cd62b2437a41b17e0cbd85aaf
ReinisKalnarajs <reinis.kalnarajs@gmail.com>
2021-02-08 21:14:46 +0200

    Initial Commit

---
+++ TrackLists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackLists
+//
+//  Created by reinis.k.kalnarajs on 08/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
